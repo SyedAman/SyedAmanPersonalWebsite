26863ad51b21c25e7078d0428618dd8c08d6703c
Syed Aman <sna274@nyu.edu>
2016-10-21 10:58:44 -0400

    generate ContactComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, NavBarComponent, AboutComponent, PageNotFoundComponent
+    AppComponent, HomeComponent, NavBarComponent, AboutComponent, PageNotFoundComponent, ContactComponent
   ],
   imports: [
     BrowserModule,
