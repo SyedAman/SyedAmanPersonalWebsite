39f6cb344528f464bcb06176a80e98419f7329aa
Syed Aman <sna274@nyu.edu>
2016-10-21 10:37:53 -0400

    generate PageNotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, NavBarComponent, AboutComponent
+    AppComponent, HomeComponent, NavBarComponent, AboutComponent, PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
