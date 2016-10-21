139af57466ce311ed4d6f3358b176b1780a29ca2
Syed Aman <sna274@nyu.edu>
2016-10-21 10:30:16 -0400

    generate AboutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, NavBarComponent
+    AppComponent, HomeComponent, NavBarComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
